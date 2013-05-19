88559f4c1d2a11df0c2c9b9538e652bb0e00ac24
Abraham Burnett <abeburnett@gmail.com>
2013-05-19 15:16:30 -0600

    Grey out upvote link when not logged in / already voted

--- server/fixtures.js
+++ server/fixtures.js
@@ -25,7 +25,8 @@ if (Posts.find().count() === 0) {
         author: sacha.profile.name,
         url: 'http://sachagreif.com/introducing-telescope/',
         submitted: now - 7 * 3600 * 1000,
-        commentsCount: 2
+        commentsCount: 2,
+        upvoters: [], votes: 0
     });
 
     Comments.insert({
@@ -50,7 +51,8 @@ if (Posts.find().count() === 0) {
         author: tom.profile.name,
         url: 'http://meteor.com',
         submitted: now - 10 * 3600 * 1000,
-        commentsCount: 0
+        commentsCount: 0,
+        upvoters: [], votes: 0
     });
 
     Posts.insert({
@@ -59,7 +61,8 @@ if (Posts.find().count() === 0) {
         userId: tom._id,
         url: 'http://themeteorbook.com',
         submitted: now - 12 * 3600 * 1000,
-        commentsCount: 0
+        commentsCount: 0,
+        upvoters: [], votes: 0
     });
 
     for (var i = 0; i < 30; i++) {
@@ -69,7 +72,8 @@ if (Posts.find().count() === 0) {
             userId: sacha._id,
             url: 'http://google.com/?q=test-' + i,
             submitted: now - i * 3600 * 1000,
-            commentsCount: 0
+            commentsCount: 0,
+            upvoters: [], votes: 0
         });
     }
 }
